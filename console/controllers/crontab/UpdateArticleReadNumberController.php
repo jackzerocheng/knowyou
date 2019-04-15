a455f6738dfda0d6edfd0a26598a270f1881c3c4
jzc <igetjzc@outlook.com>
2019-04-15 19:32:52 +0800

    脚本

--- console/controllers/crontab/UpdateArticleReadNumberController.php
+++ console/controllers/crontab/UpdateArticleReadNumberController.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Message:
+ * Message: 更新文章阅读数
  * User: jzc
  * Date: 2018/12/4
  * Time: 3:29 PM
@@ -24,17 +24,17 @@ class UpdateArticleReadNumberController extends BaseController
         $redis = Yii::$app->redis;
 
         if (!$redis->exists($key)) {
-            Yii::error("cannot get redis read number key", CATEGORIES_ERROR);
+            Yii::error("error:cannot get redis read number key", CATEGORIES_CONSOLE);
             exit(1);
         }
 
         $data = $redis->hgetall($key);//获取脚本执行这一刻的全部数据并更新
         if (count($data) < 3) {
-            Yii::warning("donot have read number data", CATEGORIES_WARN);
+            Yii::warning("warning:donot have read number data", CATEGORIES_CONSOLE);
             exit(2);
         }
 
-        Yii::info("update article read number run", CATEGORIES_INFO);
+        Yii::info("info:update article read number run", CATEGORIES_CONSOLE);
         $order = array();
         /**
          * 先将同一个表的ID归到一个子数组中
@@ -51,12 +51,12 @@ class UpdateArticleReadNumberController extends BaseController
             if (!empty($line)) {
                 $rs = (new ArticleModel())->updateBatch($key, $line, 'id');
                 if (!$rs) {
-                    Yii::error("update read number to db failed!partition:{$k}", CATEGORIES_ERROR);
+                    Yii::error("error:update read number to db failed!partition:{$k}", CATEGORIES_CONSOLE);
                 }
                 $count = $count + $rs;
             }
         }
 
-        Yii::warning("update all read number data;total:{$count}", CATEGORIES_WARN);
+        Yii::warning("warning:update all read number data;total:{$count}", CATEGORIES_CONSOLE);
     }
 }
